a9659367505f00dd008a28f5a8bf1f8c7b859a33
Geoffrey Arner <geoffarner5@gmail.com>
2022-10-22 11:08:19 -0500

    added game loop

--- src/index.js
+++ src/index.js
@@ -6,29 +6,17 @@ let ctx = canvas.getContext("2d");
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-ctx.clearRect(0, 0, 800, 600);
-
-ctx.fillStyle = "#f00";
-ctx.fillRect(20, 20, 100, 100);
-
-ctx.fillRect(200, 200, 50, 50);
-
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 
 paddle.draw(ctx);
 
-// class Paddle {
-//   constructor(gameWidth, gameHeight) {
-//     this.width = 150;
-//     this.height = 30;
+let lastTime = 0;
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+  ctx.clearRect(0, 0, 800, 600);
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
 
-//     this.position = {
-//       x: gameWidth / 2 - this.width / 2,
-//       y: gameHeight - this.height - 10,
-//     };
-//   }
-
-//   draw(ctx) {
-//     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-//   }
-// }
+  requestAnimationFrame(gameLoop);
+}
--- src/paddle.js
+++ src/paddle.js
@@ -1,7 +1,7 @@
 export default class Paddle {
   constructor(gameWidth, gameHeight) {
     this.width = 150;
-    this.height = 30;
+    this.height = 20;
 
     this.position = {
       x: gameWidth / 2 - this.width / 2,
@@ -10,6 +10,10 @@ export default class Paddle {
   }
 
   draw(ctx) {
+    ctx.fillStyle = "#0ff";
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  update(deltaTime) {
+    this.position.x += 5 / deltaTime;
   }
 }
